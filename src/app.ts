d2f2b0051e11349a3b4d036a47aea1aded879585
Yuka <yukamoribe@outlook.com>
2021-02-15 15:27:23 -0500

    Create HttpException child class to define error type with TS.

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,8 @@ const cookieParser = require('cookie-parser')
 
 // Routes
 const router = require('./routes/index.ts')
-
+const { Request, Response, NextFunction } = require('express')
+const HttpException = require('./utils/HttpException')
 // load the environment variables from the .env file
 // dotenv.config({
 //   path: '.env'
@@ -26,9 +27,42 @@ app.use(express.urlencoded({ extended: false})) // ???
 
 app.use(router)
 
+app.use((_req, _res, next) => {
+  const err = new Error('The requested resource couldn\'t be found.')
+
+  err.errors = ['The requested resource couldn\'t be found.']
+  err.status = 404
+  next(err)
+});
 
 // ERROR HANDLERS
 
+app.use((err: HttpException, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map(e => e.message);
+    err.title = 'Sequelize Error';
+  }
+  next(err);
+});
+
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  const isProduction = environment === 'production';
+
+  const errorData = {
+    title: err.title || 'Server Error',
+    message: err.message,
+    stack: isProduction ? null : err.stack,
+    errors: err.errors
+  };
+
+  if (err.status === 401) {
+    res.set('WWW-Authenticate', 'Bearer');
+  }
+  
+  console.error(errorData);
+  res.json(errorData);
+});
 
 module.exports = app
 
